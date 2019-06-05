8820b96aba4373736be94a204cada044537aebe1
KHaoc <2427870449@qq.com>
2019-06-05 20:20:50 +0800

    add

---
+++ demo.java
@@ -0,0 +1,20 @@
+public class de_mo{
+    final int abc=0;
+    int t_T;
+    void fun_asd(int i){
+        int j=i;
+    }
+    public static void main0(String [] args){
+        aas_qweq_sad a= new aas_qweq_sad();
+        fun_asd(3);
+        int j=abc;
+        t_T=9;
+        for(int i=1;i<=10;i++){
+            j++;
+        }
+                }
+}
+public class aas_qweq_sad{
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+//public class Main {
+//
+//    public static void main(String[] args) {
+//	// write your code here
+//    }
+//}
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static Queue<Token> warning;
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        warning=new LinkedList<Token>();
+        String inputFile=null;
+        if(args.length>0) inputFile=args[0];
+        else return;
+        CharStream input=CharStreams.fromFileName(inputFile);
+        JavaLexer lexer=new JavaLexer(input);
+        CommonTokenStream tokens=new CommonTokenStream(lexer);
+        JavaParser parser = new JavaParser(tokens);
+        ParseTree tree = parser.compilationUnit();
+        //System.out.println(tree.toStringTree(parser));
+        ParseTreeWalker walker = new ParseTreeWalker();
+        Standardizer sta=new Standardizer(tokens);
+        walker.walk(sta,tree);
+        System.out.println();
+        System.out.println(sta.rewriter.getText());
+        while(!warning.isEmpty()){
+            CommonToken t=(CommonToken) warning.poll();
+            System.out.println("nonstandard name in line "+t.getLine()+" : "+t.getText());
+        }
+    }
+}
